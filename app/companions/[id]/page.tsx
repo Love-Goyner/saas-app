7d6d796a052014371800902a5cca5dedb8268ae1
Love Goyner <lovegoyner@gmail.com>
2025-06-11 17:43:48 +0530

    Vapi implementation

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -1,3 +1,4 @@
+import CompanionComponent from "@/components/CompanionComponent";
 import { getCompanion } from "@/lib/actions/companion.actions";
 import { currentUser } from "@clerk/nextjs/server";
 import Image from "next/image";
@@ -15,16 +16,13 @@ const CompanionSession = async ({ params }: CompanionSessionPageProps) => {
   if (!user) redirect("/sign-in");
   if (!companion) redirect("/companions");
 
-   const { name, subject, title, topic, duration } = companion;
+  const { name, subject, title, topic, duration } = companion;
 
   return (
     <main>
       <article className="flex rounded-border justify-between p-6 max-md:flex-col">
         <div className="flex items-center gap-2">
-          <div
-            className="size-[72px] flex items-center justify-center rounded-lg max-md:hidden"
-           
-          >
+          <div className="size-[72px] flex items-center justify-center rounded-lg max-md:hidden">
             <Image
               src={`/icons/${subject}.svg`}
               alt={subject}
@@ -45,6 +43,12 @@ const CompanionSession = async ({ params }: CompanionSessionPageProps) => {
           {duration} minutes
         </div>
       </article>
+      <CompanionComponent
+        {...companion}
+        companionId={id}
+        userName={user.firstName!}
+        userImage={user.imageUrl!}
+      />
     </main>
   );
 };
